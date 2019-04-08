53d02a834f0dc438dd51310b708b189e12599fb7
Keita Urashima <ursm@ursm.jp>
2019-04-08 11:22:07 +0900

    deprecation

--- __mocks__/memjs.js
+++ __mocks__/memjs.js
@@ -11,7 +11,7 @@ module.exports = {
         },
 
         async set(key, val) {
-          data.set(key, Buffer.isBuffer(val) ? val : new Buffer(val))
+          data.set(key, Buffer.from(val))
         }
       }
     }
